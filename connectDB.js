688377a6231d69c2c115bd2baeb2cad695da02ba
nacon304 <nhatnguyen106225@gmail.com>
2024-06-04 15:12:04 +0700

    code db

--- connectDB.js
+++ connectDB.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
+const connectionParam = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb+srv://admin:3erkkrKTy2EJBjUl@webdev-21tn-group7.dmacvr7.mongodb.net/?retryWrites=true&w=majority&appName=WebDev-21TN-Group7')
   .then(() => console.log('Connected!'));
+
+  // const { db } = mongoose.connection;
+  // const result = await db.collection('test').find().toArray();
+
+const Schema = mongoose.Schema;
+
+const account = new Schema({
+  Email: String,
+  Password: String,
+  IsVerified: Boolean,
+}
+, {
+  collection: 'tms-v00'
+}
+);
+
+const AccountModel = mongoose.model('Account', account);
+
+AccountModel.find({})
+.then(function(data) {
+  console.log("data", data)
+})
+.catch(function(err){
+  console.log(err)
+})
