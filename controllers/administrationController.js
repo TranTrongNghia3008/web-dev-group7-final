09112b87b948cf08ff0804b75b4388173e10163b
TranTrongNghia3008 <nghia.trantrong3008@gmail.com>
2024-06-10 02:32:48 +0700

    update administration

--- controllers/administrationController.js
+++ controllers/administrationController.js
@@ -8,6 +8,9 @@ const controller = {};
 const userModel = require('../models/userModel');
 
 controller.show = async (req, res) => {
+    let userTypeKeyword = req.query.userTypeKeyword || '';
+    let userStatusKeyword = req.query.userStatusKeyword || '';
+    let userKeyword = req.query.userKeyword || '';
     let options = {};
     // Sort
     // let sortby = req.query.sortby || 'Name';
@@ -24,7 +27,24 @@ controller.show = async (req, res) => {
     options.skip = skip;  
     console.log(options);
 
-    const users = await userModel.find(null, null, options);     //console.log(users);
+    let query = {};
+
+    query.Name = { $regex: userKeyword, $options: 'i' };
+    query.AccountEmail = { $regex: userKeyword, $options: 'i' }
+    // Filter by userTypeKeyword
+    if (userTypeKeyword) {
+        if (userTypeKeyword === 'Admin') {
+            query.IsAdmin = true;
+        } else if (userTypeKeyword === 'User') {
+            query.IsAdmin = false;
+        }
+    }
+    if (userStatusKeyword && userStatusKeyword !== 'All') {
+        query.Status = userStatusKeyword;
+    }
+
+
+    const users = await userModel.find(query, null, options);     //console.log(users);
     const usersCount = await userModel.countDocuments(); 
     res.locals.pagination = 
     {
@@ -35,6 +55,11 @@ controller.show = async (req, res) => {
         queryParams: req.query
     };
 
+    // Calculate total, active, and inactive users
+    const totalUser = await userModel.countDocuments();
+    const activeUser = await userModel.countDocuments({ Status: 'Active' });
+    const inactiveUser = await userModel.countDocuments({ Status: 'Inactive' });
+
 
     res.render('administration', { 
         title: "ShareBug - Administration", 
@@ -42,6 +67,11 @@ controller.show = async (req, res) => {
                 <link rel="stylesheet" href="/css/administration-view.css" />`,
         d3: "selected-menu-item", 
         data: users,
+        totalUser,
+        activeUser,
+        inactiveUser,
+        userTypeKeyword,
+        userStatusKeyword,
     });
 }
 
