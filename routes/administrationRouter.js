1f28141b6c86a5d9bca737a6f17a5efcf1016597
TranTrongNghia3008 <nghia.trantrong3008@gmail.com>
2024-06-23 04:36:54 +0700

    push admin CRUD

--- routes/administrationRouter.js
+++ routes/administrationRouter.js
@@ -3,15 +3,19 @@
 const express = require('express');
 const controller = require('../controllers/administrationController');
 const router = express.Router();
+const upload = require('../middlewares/upload');
 
 
 
 router.get('/', controller.show);
-router.get('/:page', controller.show);
 router.get('/add-user', controller.showAddUser);
+router.get('/:page', controller.show);
+
 //router.get('/:sortby:order', controller.show);
 
-
+router.post('/', upload, controller.addUser);
+router.put('/', upload, controller.editUser);
+router.delete('/:id', controller.deleteUser);
 
 
 module.exports = router;
