ed526573e216b47d0c5be40c6df729c628f4e78b
quoc-trung-nguyen-hcmus <121560414+quoc-trung-nguyen-hcmus@users.noreply.github.com>
2024-07-15 12:39:48 +0700

    Fixed pagination for TestCases.show, Administration.show, ProjectList.show

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -63,16 +63,24 @@ controller.showList = async (req, res) => {
 
         // Pagination
         let total = projectsWithDetails.length;
-        let limit = 5;
-        let invalidPage = isNaN(req.query.page) || req.query.page < 1 || req.query.page > Math.ceil(total / limit);
+        let limit = 6;
+        let page = 1;
+        // Validate page query 
+        let invalidPage = isNaN(req.query.page) 
+        || req.query.page < 1 
+        || (req.query.page > Math.ceil(total / limit) && total > 0)
+        || (req.query.page > 1 && total == 0);
         if (invalidPage) {
-            return res.redirect('/project/list?page=1');
+            // Redirect to the first page
+            return res.redirect(`/project/list?page=1`);
         }
-        let page = isNaN(req.query.page) ? 1 : Math.max(1, parseInt(req.query.page));
+        else
+        {
+            page = isNaN(req.query.page) ? 1 : Math.max(1, parseInt(req.query.page));
+        }
         let skip = (page - 1) * limit;
-        
-        let showing = Math.min(limit, total - skip);
-        res.locals.pagination =
+        let showing = Math.min(total, skip + limit);
+        res.locals.pagination = 
         {
             page: page,
             limit: limit,
@@ -80,6 +88,8 @@ controller.showList = async (req, res) => {
             totalRows: total,
             queryParams: req.query
         };
+        // end Pagination
+
 
         const account = req.user;
         const user = await userModel.findOne({ AccountEmail: account.Email });
